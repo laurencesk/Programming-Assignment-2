cf9c9b813b8e5382d2cc0594d3915f0fbb04c1aa
Laurence <laurencesk@gmail.com>
2016-08-27 00:35:32 -0700

    Pushing

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,24 @@
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
+  m <- NULL #sets the value of m to null
   set <- function(y) {
     x <<- y
     m <<- NULL
-  }
-  get <- function() x
-  setinv <- function(solve) m <<- solve
-  getinv <- function() m
+  } # set the value of the matrix
+  get <- function() x # get the value of the maxtrix
+  setinv <- function(solve) m <<- solve # set the value of the inverse
+  getinv <- function() m # get the value of the inverse
   list(set = set, get = get,setinv = setinv,getinv = getinv)
 }
 
 cacheSolve <- function(x, ...) {
 
-  m <- x$getinv()
+  m <- x$getinv()  # get inverse of matrix if it already exits
   if(!is.null(m)) {
     message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinv(m)
+    return(m) # check that matrix hasn't changed, and if it hasn't, sends a text message and returns the cached matrix
+      }
+  data <- x$get() # get the value of the input matrix
+  m <- solve(data, ...) # compute the value of the inverse of the input matrix
+  x$setinv(m)  # cache the inverse
   m
 }
